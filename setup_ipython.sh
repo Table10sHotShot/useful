65cae3f161b21435951e5ac1c81505606eb2cbb7
Alex Vig <jalexvig@gmail.com>
2018-07-26 01:10:31 -0400

    Create an ipython config before modifying it

--- setup_ipython.sh
+++ setup_ipython.sh
@@ -1,8 +1,12 @@
 if [ ! -f $HOME/.ipython/profile_default/startup/ipython_startup.py ]; then
-    echo ipython_startup does not exist in $HOME/.ipython/profile_default/startup... symlinking it now
-    ln -s $PWD/ipython_startup.py $HOME/.ipython/profile_default/startup/ipython_startup.py
+  echo ipython_startup does not exist in $HOME/.ipython/profile_default/startup... symlinking it now
+  ln -s $PWD/ipython_startup.py $HOME/.ipython/profile_default/startup/ipython_startup.py
 else
-    echo ipython_startup already exists in $HOME/.ipython/profile_default/startup
+  echo ipython_startup already exists in $HOME/.ipython/profile_default/startup
+fi
+
+if [ ! -f ~/.ipython/profile_default/ipython_config.py ]; then
+  ipython profile create
 fi
 
 if grep -Fxqf $PWD/ipython_config.py ~/.ipython/profile_default/ipython_config.py; then
